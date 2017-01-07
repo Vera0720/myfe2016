1374cb528e9153199b1c9f78f9d43f580501087f
Vera0720 <1147487144@qq.com>
2017-01-07 14:07:32 +0800

    文本框高度变化

---
+++ jquery/文本框高度变化.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/1/6.
+ */	$(function(){
+    var $comment = $('#comment');  //获取评论框
+    $('.bigger').click(function(){ //放大按钮绑定单击事件
+        $comment.height($comment.height()<500?$comment.height()+50:$comment.height());
+    });
+    $('.smaller').click(function(){//缩小按钮绑定单击事件
+        $comment.height($comment.height()>50?$comment.height()-50:$comment.height());
+    });
+});
